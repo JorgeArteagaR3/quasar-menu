f51d9144c73c073ae3409988dceff12a078b25f2
Jore Arteaga <jorge.aranibar26@gmail.com>
2023-04-20 16:52:05 -0500

    cambie el url de roles

--- src/data.js
+++ src/data.js
@@ -221,7 +221,7 @@ export const dataBase = [
             },
             {
                 subtitle: "Roles",
-                url: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F8xoVBEeTqECt5pmLDHCDcR%2FQuasar%252FGestionar-Roles%3Fpage-id%3D0%253A1%26node-id%3D1%253A137%26viewport%3D358%252C121%252C0.34%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A137",
+                url: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F8xoVBEeTqECt5pmLDHCDcR%2FQuasar%252FRoles%3Fnode-id%3D108-2%26scaling%3Dcontain%26page-id%3D0%253A1%26starting-point-node-id%3D108%253A2",
             },
         ],
     },
