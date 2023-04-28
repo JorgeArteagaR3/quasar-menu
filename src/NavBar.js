96ea27ac711005715f18f8444c9645d46472c977
Jore Arteaga <jorge.aranibar26@gmail.com>
2023-04-28 01:18:48 -0500

    base de datos ordenado alfabeticamente

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,11 +2,13 @@ import Logo from "./assets/logo.png";
 import NavSection from "./NavSection";
 import { reportes, modulos, administracion, dataBase } from "./data";
 import { AiOutlinePushpin } from "react-icons/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+    const sortedDatabase = dataBase.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+    });
     return (
         <aside className={isMenuOpen ? "menu wrapped" : "menu"}>
             <AiOutlinePushpin
@@ -17,28 +19,13 @@ const NavBar = () => {
             />
             <nav className={isMenuOpen && "inactive"}>
                 <div className="logo">
-                    <img
-                        src={Logo}
-                        alt="logo"
-                    ></img>
+                    <img src={Logo} alt="logo"></img>
                     <h1>Quasar</h1>
                 </div>
-                <NavSection
-                    title={"REPORTES"}
-                    list={reportes}
-                />
-                <NavSection
-                    title={"MODULOS"}
-                    list={modulos}
-                />
-                <NavSection
-                    title={"ADMINISTRACIÓN"}
-                    list={administracion}
-                />
-                <NavSection
-                    title={"BASE DE DATOS"}
-                    list={dataBase}
-                />
+                <NavSection title={"REPORTES"} list={reportes} />
+                <NavSection title={"MODULOS"} list={modulos} />
+                <NavSection title={"ADMINISTRACIÓN"} list={administracion} />
+                <NavSection title={"BASE DE DATOS"} list={sortedDatabase} />
             </nav>
         </aside>
     );
